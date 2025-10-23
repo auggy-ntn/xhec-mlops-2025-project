01faecba8dfd6c9cfb22d214fbca06c3ebe6ace0
Augustin <augustin.naton@gmail.com>
2025-10-23 17:19:42 +0200

    Reworked utils functions

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -2,7 +2,7 @@
 
 import pickle as pkl
 from pathlib import Path
-from typing import Any, Union
+from typing import Any
 
 import pandas as pd
 
@@ -26,32 +26,29 @@ def load_data(file_path: str = DATA_DIR / "abalone.csv") -> pd.DataFrame:
     return pd.read_csv(file_path)
 
 
-def save_to_pickle(obj: Any, file_path: Union[str, Path]) -> None:
+def save_to_pickle(obj: Any, file_path: str) -> None:
     """Save an object to a specified file path in pickle format.
 
     Args:
         obj (object): The object to pickle.
-        file_path (str | Path): The file path where the object will be saved.
+        file_path (str): The file path where the object will be saved.
 
     Returns:
         None
     """
-    file_path = Path(file_path)
-    file_path.parent.mkdir(parents=True, exist_ok=True)
-    with file_path.open("wb") as f:
+    with Path.open(file_path, "wb") as f:
         pkl.dump(obj, f)
 
 
-def load_from_pickle(file_path: Union[str, Path]) -> Any:
+def load_from_pickle(file_path: str) -> Any:
     """Load an object from a specified pickle file.
 
     Args:
-        file_path (str | Path): The file path from which the object will be loaded.
+        file_path (str): The file path from which the object will be loaded.
 
     Returns:
         Any: The unpickled object.
     """
-    file_path = Path(file_path)
-    with file_path.open("rb") as f:
+    with Path.open(file_path, "rb") as f:
         obj = pkl.load(f)
     return obj
